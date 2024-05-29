027cd1842101eea8e46629dbd75649a281b20cb1
Super123qew <167475302+Super123qew@users.noreply.github.com>
2024-05-29 12:54:32 -0400

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -12,16 +12,24 @@ join Conferences c on t.conference_id = c.conference_id
 order by c.conference_name, t.team_name;
 
 --Aggregation and grouping of data by finding the avg points scored in a match and finding the avg height/weight by position
-SELECT t.team_name, CEILING(AVG(s.points)) AS avg_points
+SELECT t.team_name, g.game_id, g.game_date, CEILING(AVG(s.points)) AS avg_points
 FROM Teams t
 JOIN Players p ON t.team_id = p.team_id
 JOIN Statistics s ON p.player_id = s.player_id
-GROUP BY t.team_name;
+JOIN Games g ON g.game_id = s.game_id
+GROUP BY t.team_name, g.game_id, g.game_date
+ORDER BY t.team_name, g.game_date;
 
 
-select p.position, AVG(p.height) as avg_height, AVG(p.weight) as avg_weight
-from Players p
-group by p.position;
+SELECT p.player_name, p.position, averages.avg_height, averages.avg_weight
+FROM Players p
+JOIN (
+  SELECT position, AVG(height) AS avg_height, AVG(weight) AS avg_weight
+  FROM Players
+  GROUP BY position
+) AS averages ON p.position = averages.position
+ORDER BY p.position, p.player_name;
+
 
 --Joining of data by connecting multiple tables of team info into one row
 select p.player_name, t.team_name, t.city, a.arena_name, c.coach_name
